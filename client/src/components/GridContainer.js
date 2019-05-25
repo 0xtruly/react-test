6e2a5b64664f7accdc5225497b8de650cd5f694d
Segun <adebanjosegun589@gmail.com>
2019-05-25 06:56:26 +0100

    load more data on scroll and load new advert

--- client/src/components/GridContainer.js
+++ client/src/components/GridContainer.js
@@ -1,7 +1,7 @@
 
 import styled from 'styled-components';
 
-export const Grid = styled.div`
+export const Ul = styled.ul`
     display: grid;
     grid-template-columns: repeat(3,1fr);
     grid-gap: 30px;
